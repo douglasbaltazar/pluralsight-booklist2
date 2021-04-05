95d754e11bf6728e279354f7a7e167d7b8a98c43
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-04-05 13:18:42 -0300

    Segunda parte do curso

---
+++ src/components/BookForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <form
+    v-on:submit.prevent="bookSubmit(bookData)"
+    class=""
+    action="#"
+    method="post"
+  >
+    <input
+      v-model="bookData.bookTitle"
+      type="text"
+      name="title"
+      value=""
+      placeholder="Book Title"
+    />
+    <input
+      v-model="bookData.bookAuthor"
+      type="text"
+      name="author"
+      value=""
+      placeholder="Book Author"
+    />
+    <div>
+      <label for="finishedReading">Finished Reading</label>
+      <input type="checkbox" v-model="bookData.finishedReading" />
+    </div>
+    <div>
+      <label for="borrowed">Borrowed</label>
+      <input type="radio" v-model="bookData.ownership" value="borrowed" />
+      <label for="bought">Bought</label>
+      <input type="radio" v-model="bookData.ownership" value="bought" />
+    </div>
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: [],
+      },
+    };
+  },
+  methods: {
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
